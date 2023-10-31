ca4f4da20d0aa1bc5ad8e209900af7ff5a6ae1c4
akioers <andre.saraiva@carlsberggroup.com>
2023-10-31 15:30:04 +0000

    feat: adds new operations

--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -1,23 +1,35 @@
 package calculator;
 
 public class Calculator {
-  public static double add(double x, double y) {
-    return x + y;
-  }
-
-  public static double subtract(double x, double y) {
-    return x - y;
-  }
-
-  public static double multiply(double x, double y) {
-    return x * y;
-  }
-
-  public static double divide(double x, double y) {
-    if (y == 0) {
-      System.out.println("Cannot divide by 0");
-      return 0;
+    public static double add(double x, double y) {
+        return x + y;
     }
-    return x / y;
-  }
+
+    public static double subtract(double x, double y) {
+        return x - y;
+    }
+
+    public static double multiply(double x, double y) {
+        return x * y;
+    }
+
+    public static double divide(double x, double y) {
+        if (y == 0) {
+            System.out.println("Cannot divide by 0");
+            return 0;
+        }
+        return x / y;
+    }
+
+    public static double power(double x, double y) {
+        return Math.pow(x, y);
+    }
+
+    public static double root(double x) {
+        return Math.sqrt(x);
+    }
+
+    public static double log(double x) {
+        return Math.log(x);
+    }
 }
